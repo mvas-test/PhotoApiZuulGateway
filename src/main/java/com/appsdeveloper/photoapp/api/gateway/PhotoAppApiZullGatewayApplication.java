bcd1d232943c1f136705ccf06d61b60d5c94b4a9
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-05-21 14:31:50 +0100

    first commit

---
+++ src/main/java/com/appsdeveloper/photoapp/api/gateway/PhotoAppApiZullGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloper.photoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiZullGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiZullGatewayApplication.class, args);
+	}
+
+}
