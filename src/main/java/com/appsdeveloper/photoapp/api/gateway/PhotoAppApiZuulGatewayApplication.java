f3aedd4a6a17806973f8ce961129289ffb1733e0
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-06-07 22:29:31 +0100

    Enable Zuul Proxy and Enable Eureka Client

--- src/main/java/com/appsdeveloper/photoapp/api/gateway/PhotoAppApiZuulGatewayApplication.java
+++ src/main/java/com/appsdeveloper/photoapp/api/gateway/PhotoAppApiZuulGatewayApplication.java
@@ -2,8 +2,12 @@ package com.appsdeveloper.photoapp.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class PhotoAppApiZuulGatewayApplication {
 
 	public static void main(String[] args) {
