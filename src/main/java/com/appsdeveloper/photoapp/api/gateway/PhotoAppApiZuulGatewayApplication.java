1ec4ec8205ffc8d0eb6dd30e411761f9df1c834c
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-06-27 13:30:02 +0100

    Added Spring Boot Actuator settings, updated rabbitMQ password and added custom properties metadata

--- src/main/java/com/appsdeveloper/photoapp/api/gateway/PhotoAppApiZuulGatewayApplication.java
+++ src/main/java/com/appsdeveloper/photoapp/api/gateway/PhotoAppApiZuulGatewayApplication.java
@@ -1,9 +1,12 @@
 package com.appsdeveloper.photoapp.api.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,5 +16,10 @@ public class PhotoAppApiZuulGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiZuulGatewayApplication.class, args);
 	}
+	
+	@Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 
 }
